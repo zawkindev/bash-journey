10da576dd864aad7c7f88be5c94d7883ad4bbda2
zawkindev <zawkindev@gmail.com>
2024-03-15 10:24:25 +0500

    refactor: change code

--- rw.sh
+++ rw.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
-#write to file
-echo "Goodbye world" >> bye.txt # this appends to end of file
+# append to file
+echo "appending..."
+echo "Good bye world" >> bye.txt # this appends text to end of by.txt 
 
 #read from file
 while read -r line; do
   echo $line
 done < bye.txt 
 
-#write to file
-echo "I overwrote bye.txt" > bye.txt # this overwrites bye.txt
+echo "---"
+
+#overwrite file
+echo "overwriting..."
+echo "Sayounara" > bye.txt # this overwrites bye.txt
+
+while read -r line; do
+  echo $line
+done < bye.txt 
+
 
 
 
 # run code:
 
+
 # [bash-journey]$ ls
 # io.sh  README.md  rw.sh
-# [bash-journey]$ sh rw.sh 
-# Goodbye world
-# [bash-journey]$ ls
-# bye.txt  io.sh  README.md  rw.sh    // bye.txt created
-# [bash-journey]$ cat bye.txt 
-# I overwrote bye.txt
-# [bash-journey]$ 
 
+# [bash-journey]$ sh rw.sh 
+# appending...
+# Good bye world
+# ---
+# overwriting...
+# Sayounara
+
+# [bash-journey]$ ls
+# bye.txt  io.sh  README.md  rw.sh
+
+# [bash-journey]$ cat bye.txt 
+# Sayounara
+
+# [bash-journey]$ 
